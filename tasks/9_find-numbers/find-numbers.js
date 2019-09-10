de8349bd94177e8b1fdf68b01e17f5400ff32478
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-10 15:07:50 +0400

    Task 9 post-review fix

--- tasks/9_find-numbers/find-numbers.js
+++ tasks/9_find-numbers/find-numbers.js
@@ -2,7 +2,7 @@ function getNumbers(str) {
   const regex = /\d+/g;
   let digitsArr = str.match(regex);
   for (let index = 0; index < digitsArr.length; index++) {
-    digitsArr[index] = +digitsArr[index];
+    digitsArr[index] = Number(digitsArr[index]);
   }
   return digitsArr;
 }
