ab367ebb512291dae64ccaf301adedd2df959d60
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-10 15:08:00 +0400

    Task 10 post-review fix

--- tasks/10_remove-whitespaces/remove-whitespaces.js
+++ tasks/10_remove-whitespaces/remove-whitespaces.js
@@ -1,11 +1,5 @@
 function trimWhiteSpaces(str) {
-  let tempArr = str.split(' ');
-  for (let i = 0; i < tempArr.length; i++) {
-    if (tempArr[i] == ' ') {
-      tempArr.splice(i, 1);
-    }
-  }
-  return tempArr.join('');
+  return str.replace(/ /g, '');
 }
 
 export {trimWhiteSpaces};
