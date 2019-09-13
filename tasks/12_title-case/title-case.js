d25862fa755d8f2a4db3407d3ecb6401db43b068
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-13 16:21:04 +0400

    fix: minor style fixes

--- tasks/12_title-case/title-case.js
+++ tasks/12_title-case/title-case.js
@@ -1,7 +1,8 @@
 function titleCase(str) {
   let res = str.split(' ');
-  res = res.map((item) =>
-    item = item[0].toUpperCase() + item.slice(1).toLowerCase());
+  res = res.map(
+      (item) => item = item[0].toUpperCase() + item.slice(1).toLowerCase()
+  );
   return res.join(' ');
 }
 export {titleCase};
