14c5f60b1949d2fa28fb8976b0b620647b913f4e
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-13 13:42:57 +0400

    Task 38

---
+++ tasks/38_max-product/max-product.js
@@ -0,0 +1,9 @@
+function maxProduct(arr) {
+  const multis = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    multis.push(arr[i]*arr[i+1]);
+  }
+  return multis.sort((a, b) => a - b).pop();
+}
+
+export {maxProduct};
