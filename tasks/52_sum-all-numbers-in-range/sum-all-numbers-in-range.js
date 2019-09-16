80eef8efe82c4e6caaf10e810aa32b23bd12b891
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-16 19:27:53 +0400

    Task 52

---
+++ tasks/52_sum-all-numbers-in-range/sum-all-numbers-in-range.js
@@ -0,0 +1,11 @@
+function sumAll(arr) {
+  const max = Math.max(arr[0], arr[1]);
+  const min = Math.min(arr[0], arr[1]);
+  let res = 0;
+  for (let i = min; i <= max; i++) {
+    res += i;
+  }
+  return res;
+}
+
+export {sumAll};
