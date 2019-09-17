e61b509fcb884d99590f0752c17f70619c517aee
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-17 16:05:48 +0400

    fix: task 37 refactoring

--- tasks/34_max-triplet-sum/max-triplet-sum.js
+++ tasks/34_max-triplet-sum/max-triplet-sum.js
@@ -1,4 +1,5 @@
-function maxTripletSum(arr) {
+function maxTripletSum(input) {
+  const arr = input.slice();
   arr.sort((a, b) => b - a);
   const resArr = [];
   arr.forEach((item) => !resArr.includes(item) && resArr.push(item));
--- tasks/37_min-number-to-prime/min-number-to-prime.js
+++ tasks/37_min-number-to-prime/min-number-to-prime.js
@@ -1,29 +1,17 @@
 function minNumberToPrime(arr) {
-  const res = arr.reduce(
-      (sum, item) => sum + item, 0);
-  return primesToNum(res).pop() - res;
-}
-
-function primesToNum(num) {
-  const primes = [];
-  let isPrime;
-  for (let i = 2; i < Math.pow(num, 2); i++) {
+  const sum = arr.reduce((sum, item) => sum + item, 0);
+  let isPrime = true;
+  for (let i = sum; i < Math.pow(sum, 2); i++) {
     for (let j = 2; j < i; j++) {
       if (i % j === 0) {
         isPrime = false;
-        break;
       }
     }
-    if (!isPrime) {
-      isPrime = true;
-      continue;
+    if (isPrime) {
+      return i - sum;
     }
-    primes.push(i);
-    if (i >= num) {
-      break;
-    }
+    isPrime = true;
   }
-  return primes;
 }
 
 export {minNumberToPrime};
