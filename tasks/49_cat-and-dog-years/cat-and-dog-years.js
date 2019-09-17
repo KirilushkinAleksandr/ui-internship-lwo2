bd57c5cce4e06575ab47d57a9a0ca52d04f80e8b
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-17 14:23:15 +0400

    fix: alexkond review fix

--- tasks/49_cat-and-dog-years/cat-and-dog-years.js
+++ tasks/49_cat-and-dog-years/cat-and-dog-years.js
@@ -1,37 +1,14 @@
 function humanYearsCatYearsDogYears(years) {
-  return [years,catAge(years),dogAge(years)];
-}
-
-function catAge(years) {
-  let age = 0;
-  for (let i = 1; i <= years; i++) {
-    if (i === 1){
-      age += 15;
-      continue;
-    }
-    if (i === 2) {
-      age += 9;
-      continue;
-    }
-    age += 4;
+  let catAge = 15;
+  let dogAge = 15;
+  if (years >= 2) {
+    catAge = dogAge += 9;
   }
-  return age;
-}
-
-function dogAge(years) {
-  let age = 0;
-  for (let i = 1; i <= years; i++) {
-    if (i === 1){
-      age += 15;
-      continue;
-    }
-    if (i === 2) {
-      age += 9;
-      continue;
-    }
-    age += 5;
+  for (let i = 3; i <= years; i++) {
+    catAge += 4;
+    dogAge += 5;
   }
-  return age;
+  return [years, catAge, dogAge];
 }
 
 export {humanYearsCatYearsDogYears};
