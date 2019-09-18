ebb1d5baf58a56d507fec408d16e0ed7eccd83d8
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-18 21:02:13 +0400

    Task 60

---
+++ tasks/60_convert-html-entities/convert-html-entities.js
@@ -0,0 +1,10 @@
+function convertHTML(input) {
+  return input
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&apos;');
+}
+
+export {convertHTML};
