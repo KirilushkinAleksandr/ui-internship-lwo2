92fdc33c1757a6f4848cc59f28bd2e8b9a965e5a
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-18 19:53:19 +0400

    fix: Task 58

--- tasks/58_missed-letters/missed-letters.js
+++ tasks/58_missed-letters/missed-letters.js
@@ -1,24 +1,11 @@
 function findLetter(str) {
   const lets = str.split('');
-  const start = lets.charCodeAt(0);
+  const startCode = lets[0].charCodeAt(0);
   for (let i = 1; i < lets.length; i++) {
-    if (lets.charCodeAt(i) !== start + i) {
-      lets.splice(i, 0,
-          missed(lets.charCodeAt(i),
-              lets.charCodeAt(i-1)
-          )
-      );
+    if (lets[i].charCodeAt(0) !== startCode + i) {
+      return String.fromCharCode(startCode + i);
     }
   }
-  return lets.join('');
-}
-
-function missed(start, end) {
-  const res = [];
-  for (let i = start + 1; i < end; i++) {
-    res.push(String.fromCharCode(i));
-  }
-  return res.join('');
 }
 
 export {findLetter};
