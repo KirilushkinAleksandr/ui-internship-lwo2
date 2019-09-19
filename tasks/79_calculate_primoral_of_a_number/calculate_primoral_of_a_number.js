d213db10af0178387611a327166144f4e2a3bc67
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-19 23:24:46 +0400

    Task 79

---
+++ tasks/79_calculate_primoral_of_a_number/calculate_primoral_of_a_number.js
@@ -0,0 +1,23 @@
+function numPrimorial(input) {
+  let isPrime = true;
+  let res = 1;
+  let count = input;
+  for (let i = 2; i < Math.pow(input, input); i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+      }
+    }
+    if (isPrime) {
+      res *= i;
+      count--;
+    }
+    isPrime = true;
+    if (count === 0) {
+      break;
+    }
+  }
+  return res;
+}
+
+export {numPrimorial};
