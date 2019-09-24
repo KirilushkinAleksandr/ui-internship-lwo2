641837f09feb2919df382a335a4e5d8a758d4fbf
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-24 13:30:55 +0400

    Task 96

---
+++ tasks/96_greatest-common-divisor/greatest-common-divisor.js
@@ -0,0 +1,35 @@
+function greatestCommonDivisor(input1, input2) {
+  return gcd(input1, input2);
+}
+
+function gcd(m, n) {
+  if (m === 0 || n === 0) {
+    return Math.sqrt(Math.pow(m - n, 2));
+  }
+
+  if (m === n) {
+    return m;
+  }
+
+  if (m === 1 || n === 1) {
+    return 1;
+  }
+
+  if (m % 2 === 0) {
+    if (n % 2 === 0) {
+      return 2 * gcd(m / 2, n / 2);
+    } else return gcd(m / 2, n);
+  }
+
+  if (n % 2 === 0) {
+    if (m % 2 !== 0) {
+      return gcd(m, n / 2);
+    }
+  }
+
+  if (n > m) {
+    return gcd((n - m) / 2, m);
+  } else return gcd((m - n) / 2, n);
+}
+
+export {greatestCommonDivisor};
