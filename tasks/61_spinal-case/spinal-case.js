c7031f2e73d0fbb2eccba0b273d175245cef7646
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-26 02:03:12 +0400

    fix: alexkond && vovcyan review fix

--- tasks/61_spinal-case/spinal-case.js
+++ tasks/61_spinal-case/spinal-case.js
@@ -3,27 +3,14 @@
  * // return "this-is-spinal-tap"
  * spinalCase('This Is Spinal Tap')
  * function spinalCase converts a string to spinal case
- * @param {String} str in some non-spinal case
+ * @param {String} input in some non-spinal case
  * @return {String} string in spinal case
  */
-function spinalCase(str) {
-  const separators = [' ', '_'];
-  const arr = str.split('');
-  arr.map((_item, i) => {
-    if (arr[0] === arr[0].toUpperCase()) {
-      arr[0] = arr[0].toLowerCase();
-    }
-    if (separators.includes(arr[i])) {
-      arr[i] = '-';
-    } else if (arr[i] === arr[i].toUpperCase() && arr[i] !== '-') {
-      arr[i] = arr[i].toLowerCase();
-      if (arr[i-1] !== '-') {
-        arr.splice(i, 0, '-');
-        i++;
-      }
-    }
-  });
-  return arr.join('');
+function spinalCase(input) {
+  let str = input;
+  const regExp = new RegExp(/([a-z])([A-Z])/g);
+  str = str.replace(regExp, `$1 $2`);
+  return str.replace(/[\s_]/g, '-').toLowerCase();
 }
 
 export {spinalCase};
--- tasks/65_smaller-common-multiple/smaller-common-multiple.js
+++ tasks/65_smaller-common-multiple/smaller-common-multiple.js
@@ -10,7 +10,7 @@
  */
 function smallestCommons(arr) {
   let start = Math.min(arr[0], arr[1]);
-  let finish = Math.max(arr[0], arr[1]);
+  const finish = Math.max(arr[0], arr[1]);
   for (let i = start; i <= finish; i++) {
     let tempIndex = i;
     let tempRes = start;
