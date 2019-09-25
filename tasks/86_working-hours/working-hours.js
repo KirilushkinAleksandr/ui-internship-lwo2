6989d5ee17b373c2ec7277f237e3e6548c93c7f8
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-26 00:49:44 +0400

    fix: Task 86 bug fix

--- tasks/86_working-hours/working-hours.js
+++ tasks/86_working-hours/working-hours.js
@@ -1,26 +1,89 @@
+/**
+ * @example
+ * // return [
+ * // 'MON - WED: 11:00 - 23:00',
+ * //'THU - FRI: 12:00 - 23:00',
+ * //'SAT: 10:00 - 23:00',
+ * //'SUN: 11:00 - 23:00'
+ * // ];
+ * formatWorkingHours([
+ * {
+ *    "day": "sat",
+ *     "from": "10:00",
+ *     "to": "23:00"
+ * },
+ * {
+ *     "day": "mon",
+ *     "from": "11:00",
+ *     "to": "23:00"
+ * },
+ * {
+ *     "day": "tue",
+ *     "from": "11:00",
+ *     "to": "23:00"
+ * },
+ * {
+ *     "day": "wed",
+ *     "from": "11:00",
+ *     "to": "23:00"
+ * },
+ * {
+ *     "day": "thu",
+ *     "from": "12:00",
+ *     "to": "23:00"
+ * },
+ * {
+ *     "day": "fri",
+ *     "from": "12:00",
+ *     "to": "23:00"
+ * },
+ * {
+ *     "day": "sun",
+ *     "from": "11:00",
+ *     "to": "23:00"
+ * }
+ * ];)
+ * function formatWorkingHours which formats a working hours schedule,
+ * given as an array of objects, in a human-friendly way
+ * @param {Array} inputArr, unsorted array of daily schedules
+ * @return {Array} array of objects
+ */
 function formatWorkingHours(inputArr) {
-  const arr = inputArr.slice();
-  const temp = arr.shift();
-  arr.push(temp);
-  let day = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i]['from'] !== day['from'] || arr[i]['to'] !== day['to']) {
-      const startIndex = arr.indexOf(day);
-      arr.splice(startIndex, i - startIndex, concatDays(arr[i - 1], arr[i]));
-      day = arr[i];
+  const daysArr = inputArr.slice();
+  const daysOfWeek = {
+    mon: 1,
+    tue: 2,
+    wed: 3,
+    thu: 4,
+    fri: 5,
+    sat: 6,
+    sun: 7,
+  };
+  daysArr.sort(
+      (a, b) => daysOfWeek[a.day] - daysOfWeek[b.day]
+  );
+  const res = [];
+  let startIndex = 0;
+  for (let i = 1; i < daysArr.length; i++) {
+    if (i === daysArr.length - 1) {
+      res.push(`${daysArr[i - 1].day.toUpperCase()}: ` +
+      `${daysArr[i - 1].from} - ${daysArr[i - 1].to}`
+      );
+      res.push(`${daysArr[i].day.toUpperCase()}: ` +
+      `${daysArr[i].from} - ${daysArr[i].to}`
+      );
+      break;
+    }
+    if (daysArr[i].from !== daysArr[i - 1].from ||
+        daysArr[i].to !== daysArr[i - 1].to) {
+      res.push(
+          `${daysArr[startIndex].day.toUpperCase()}` +
+          ` - ${daysArr[i - 1].day.toUpperCase()}: ` +
+        `${daysArr[i - 1].from} - ${daysArr[i - 1].to}`);
+      startIndex = i;
     }
   }
-  return arr.map(convertDayToString);
-}
-
-function concatDays(start, end) {
-  start['day'] = `${start['day'].toUpperCase()} - ${end['day'].toUpperCase()}`;
-  return start;
-}
-
-function convertDayToString(inputObj) {
-  return `${inputObj['day'].toUpperCase()}: ` +
-        `${inputObj['from']} - ${inputObj['to']}`;
+  return res;
 }
 
 export {formatWorkingHours};
