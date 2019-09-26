c1983568f133bbbc224053deaaef55fdb110d506
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-09-26 08:18:44 +0400

    fix: Task 91 JSDoc added && minor fix

--- tasks/91_make-a-person/make-a-person.js
+++ tasks/91_make-a-person/make-a-person.js
@@ -1,32 +1,57 @@
+/**
+ * Class to create a person
+ */
 class Person {
+  /**
+   * constructor function used to create new instances of Person
+   * @param {String} fullName, person's first and last name
+   */
   constructor(fullName) {
-    this._firstName = fullName.split(' ')[0];
-    this._lastName = fullName.split(' ')[1];
+    this.setFirstName(fullName.split(' ')[0]);
+    this.setLastName(fullName.split(' ')[1]);
   }
-
+  /**
+   * Getter returning person's first name
+   * @return {String} Person's first name value
+   */
   getFirstName() {
     return this._firstName;
   }
-
+  /**
+  * Setter to change person's first name
+  * @param {String} newFirst, new first name value
+  */
   setFirstName(newFirst) {
     this._firstName = newFirst;
   }
-
+  /**
+   * Getter returning person's last name
+   * @return {String} Person's last name value
+   */
   getLastName() {
     return this._lastName;
   }
-
+  /**
+   * Setter to change person's last name
+   * @param {String} newLast, new last name value
+   */
   setLastName(newLast) {
     this._lastName = newLast;
   }
-
+  /**
+   * Getter returning person's full name
+   * @return {String} Person's full name value
+   */
   getFullName() {
-    return this._firstName + ' ' + this._lastName;
+    return this.getFirstName() + ' ' + this.getLastName();
   }
-
+  /**
+   * Setter to change person's full name
+   * @param {String} newFull, new full name value
+   */
   setFullName(newFull) {
-    this._firstName = newFull.split(' ')[0];
-    this._lastName = newFull.split(' ')[1];
+    this.setFirstName(newFull.split(' ')[0]);
+    this.setLastName(newFull.split(' ')[1]);
   }
 }
 
